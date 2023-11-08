341bc734cd5be13ec8c0679b91345a3aba85a88f
pp2903 <priyampranshu@gmail.com>
2023-11-08 17:28:49 +0530

    Added images to the cards

--- src/Projects.js
+++ src/Projects.js
@@ -6,26 +6,32 @@ import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 
 
-
+import { useState } from 'react';
 
 const project_data = {
 
   react:[
     {
-      name:"Mark's Pizzeria",
-      url:"https://github.com/pp2903/Mark-s-Pizzeria/tree/test"
+      id:1,
+      name:"Portfolio site",
+      url:"https://github.com/pp2903/Portfolio-site",
+      img:"./pictures/Portfolio_site.png"
     }
 
   ],
 
   django:[
     {
+      id:1,
       name:"Django CRM",
-      url:"https://github.com/pp2903/DjangoCRM"
+      url:"https://github.com/pp2903/DjangoCRM",
+      img:"./pictures/DjangoCRM.png"
     },
     {
-      name:"Portfolio site",
-      url:"https://github.com/pp2903/Portfolio-site"
+      id:2,
+      name:"Mark's Pizzeria",
+      url:"https://github.com/pp2903/Mark-s-Pizzeria/tree/test",
+      img:"./pictures/MP.png"
     },
   ]
 
@@ -40,15 +46,9 @@ const Projects = () => {
     <>
      <h2 className="project-heading">Projects</h2>
 
-
-
-
-
-
-
-<ProjectTopic name="React"/>
-<ProjectTopic name="Django"/>
-    <CardComp/>
+    <ProjectTopic topic_name="React Projects" projs={project_data.react} />
+    <ProjectTopic topic_name="Django Projects" projs={project_data.django} />
+   
 
     
     </>
@@ -57,57 +57,76 @@ const Projects = () => {
   )
 }
 
+const ProjectTopic = (props)=>{
+  const  { topic_name, projs } = props
+  
+  
 
 
+
+
+  
+  const list = projs.map((proj)=>{
+    return <span key={proj.id} className='card-body zoom'>
+     <CardComp style={{display:'inline'}} name={proj.name} url={proj.url} img={proj.img}/>
+     <br/>
+    </span>
+   
+    
+  })
+
+  
+  return <span>
+  
+    <h4>{topic_name}</h4>
+    
+    
+    <div>{list}</div>
+  
+  
+  
+  </span>
+}
 
 
 const CardComp = (props)=>{
+
+
+
+  
+  const [card,setCard] = useState(false)
+  const handleCard = ()=>{
+    if(card) setCard(false)
+    else setCard(true)
+   
+  }
+  
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <CardMedia
+        sx={{ height: 165 }}
+        image={props.img}
+        title={props.name}      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        {props.name} 
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
       <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
+        <Button href={props.url} size="small">Code</Button>
+        <Button size="small" >Visit</Button>
+        <Button size="small" >Details</Button>
       </CardActions>
     </Card>
   );
 }
 
 
-const ProjectTopic = (props)=>{
 
 
-  const { name }  = props;
-  
-  return <>
-
-  <h4 >{name} projects</h4>
-  </>
-  
-}
-
-const Project = (props)=>{
-    
-
-
-
-}
 
 export default Projects
