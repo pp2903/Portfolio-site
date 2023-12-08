a34aae64db9d4a354faa49ce86d50680221df600
pp2903 <priyampranshu@gmail.com>
2023-12-08 19:08:10 +0530

    Changed top bar css

--- src/Profile.js
+++ src/Profile.js
@@ -1,24 +1,33 @@
 // import { useState } from "react"
-import { links } from './data'
-import { Typography } from "@mui/material"
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import { links } from "./data";
+import { Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
 const Social = () => {
   const newLinks = links.map((link) => {
-    return <SocialButton name={link.name} url={link.url} img={link.img} key={link.id} />;
+    return (
+      <SocialButton
+        name={link.name}
+        url={link.url}
+        img={link.img}
+        key={link.id}
+      />
+    );
   });
-  
+
   return (
     <>
-      <div className="social-bar slide-in" id="slider">{newLinks}</div>
+      <div className="social-bar slide-in" id="slider">
+        {newLinks}
+      </div>
     </>
   );
 };
 
 const SocialButton = (props) => {
   const { name, url, img } = props;
-  
+
   return (
     <>
       <a id="" className="icon-handle" href={url}>
@@ -29,33 +38,24 @@ const SocialButton = (props) => {
 };
 
 const Profile = () => {
-  
-  
-    return (
+  return (
     <>
+      <Box sx={{ flexGrow: 1, padding: "3rem 0rem" }}>
+        <Grid className="top-bar" container spacing={1}>
+          <img className="profile-pic" src="./profile.png" alt="profile p" />
+          <div className="name-heading">
+            <Grid item sx={{ alignSelf: "flex-start" }}>
+              <Typography variant="h2" gutterBottom>
+                Pranshu Priyam
+              </Typography>
 
-    <Box sx={{flexGrow: 1,padding: '3rem 0rem'}} >
-      <Grid className="top-bar" container spacing={1}>
+              <hr />
 
-      
-        <img  className="profile-pic" src="./profile.png" alt="profile p" />
-        <div className="name-heading">
-      <Grid item sx={{'alignSelf':'flex-start'}}>
-      <Typography variant="h2" gutterBottom>
-        
-          Pranshu Priyam
-      </Typography>
-        
-        
-          <hr />
-            
-          
-          <Social />
-          </Grid>
-        </div>
-      
-      </Grid>
-    </Box>
+              <Social />
+            </Grid>
+          </div>
+        </Grid>
+      </Box>
     </>
   );
 };
